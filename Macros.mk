192b35e112becbc536ef08da294b0fe95081c541
C0deH4cker <c0deh4cker@gmail.com>
2020-09-23 00:26:11 -0700

    Fix mistakes with handling the Dockerfile path

--- Macros.mk
+++ Macros.mk
@@ -563,7 +563,7 @@ endif #DOCKERFILE
 
 # If the Dockerfile doesn't have the standard name, add an argument telling
 # docker build which Dockerfile to use
-ifneq "$$($1+DOCKERFILE)" "$1+Dockerfile"
+ifneq "$$($1+DOCKERFILE)" "$1/Dockerfile"
 $1+DOCKER_BUILD_ARGS := -f $$($1+DOCKERFILE) $$($1+DOCKER_BUILD_ARGS)
 endif #Dockerfile
 
@@ -676,7 +676,7 @@ docker-build[$$($1+DOCKER_IMAGE)]: $$(BUILD)/$1/.docker_build_marker
 # Create a marker file to track last docker build time
 $$(BUILD)/$1/.docker_build_marker: $$($1+PRODUCTS) $$($1+DOCKER_BUILD_DEPS) $$(BUILD)/$1/.dir
 	$$(_V)echo "Building docker image $$($1+DOCKER_IMAGE)"
-	$$(_v)docker build -t $$($1+DOCKER_IMAGE) $$($1+DOCKER_BUILD_FLAGS) $$(dir $1+DOCKERFILE) \
+	$$(_v)docker build -t $$($1+DOCKER_IMAGE) $$($1+DOCKER_BUILD_FLAGS) $$(dir $$($1+DOCKERFILE)) \
 		&& touch $$@
 
 # Force build a docker image
@@ -685,7 +685,7 @@ docker-rebuild: docker-rebuild[$$($1+DOCKER_IMAGE)]
 # This rebuilds the docker image no matter what
 docker-rebuild[$$($1+DOCKER_IMAGE)]: | $$($1+PRODUCTS) $$($1+DOCKER_BUILD_DEPS) $$(BUILD)/$1/.dir
 	$$(_V)echo "Rebuilding docker image $$($1+DOCKER_IMAGE)"
-	$$(_v)docker build -t $$($1+DOCKER_IMAGE) $$($1+DOCKER_BUILD_FLAGS) $$(dir $1+DOCKERFILE) \
+	$$(_v)docker build -t $$($1+DOCKER_IMAGE) $$($1+DOCKER_BUILD_FLAGS) $$(dir $$($1+DOCKERFILE)) \
 		&& touch $$(BUILD)/$1/.docker_build_marker
 
 
