fedac58daadfb3c7ea8f8c0602a20dbc656ea14b
C0deH4cker <c0deh4cker@gmail.com>
2024-10-10 15:06:04 -0700

    Allow publishing ld.so with PUBLISH_LD (like PUBLISH_LIBC)
    Fixes #42

--- examples/BaseImage/Build.mk
+++ examples/BaseImage/Build.mk
@@ -1,7 +1,12 @@
 TARGET := base-image
 
-UBUNTU_VERSION := 16.04
+UBUNTU_VERSION := 18.04
+
+PUBLISH := base-image.c
+PUBLISH_BUILD := $(TARGET)
+PUBLISH_LIBC := my-libc.so
+PUBLISH_LD := my-ld.so
 
 DOCKER_IMAGE := pwnableharness-example-base-image
-DOCKER_PORTS := 1604
+DOCKER_PORTS := 1804
 DOCKER_WRITEABLE := true
