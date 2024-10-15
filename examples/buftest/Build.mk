b802ba1c7eb1901c380e2b0969a8f1a7c6fef8c8
C0deH4cker <c0deh4cker@gmail.com>
2024-10-15 00:26:08 -0700

    Fix issue with CANARY due to `intcmp` function being unavailable (#55)
    Fixes #54

--- examples/buftest/Build.mk
+++ examples/buftest/Build.mk
@@ -2,5 +2,13 @@ TARGET := buftest
 
 # NO_UNBUFFERED_STDIO := true
 
+# Enable everything else just for fun
+NX := 1
+ASLR := 1
+RELRO := 1
+CANARY := 1
+STRIP := 1
+DEBUG := 1
+
 DOCKER_IMAGE := buftest
 DOCKER_PORTS := 12345
