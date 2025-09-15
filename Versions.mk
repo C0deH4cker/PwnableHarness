c984d011e9c1a13cd1308cc312a90283388b5a4f
C0deH4cker <c0deh4cker@gmail.com>
2025-09-15 00:01:29 -0700

    Mark v2.3.1 as released

--- Versions.mk
+++ Versions.mk
@@ -7,7 +7,7 @@
 # Update any time the PwnableHarness makefiles (or anything else in the pwnmake
 # image) are changed.
 PHMAKE_VERSION  := v2.3.1
-PHMAKE_RELEASED := v2.3
+PHMAKE_RELEASED := v2.3.1
 
 # This only needs to update when there's a change that would affect the base
 # images. Changes that only affect PwnableHarness as a build system don't need
