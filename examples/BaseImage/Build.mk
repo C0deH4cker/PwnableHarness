c8512ce460a4a7ea60a13f97051ce858047428cd
C0deH4cker <c0deh4cker@gmail.com>
2025-09-26 01:17:36 -0400

    Update base image example to use new Ubuntu version 25.04

--- examples/BaseImage/Build.mk
+++ examples/BaseImage/Build.mk
@@ -1,6 +1,6 @@
 TARGET := base-image
 
-UBUNTU_VERSION := 18.04
+UBUNTU_VERSION := 25.04
 
 PUBLISH := base-image.c
 PUBLISH_BUILD := $(TARGET)
@@ -8,5 +8,5 @@ PUBLISH_LIBC := my-libc.so
 PUBLISH_LD := my-ld.so
 
 DOCKER_IMAGE := pwnableharness-example-base-image
-DOCKER_PORTS := 1804
+DOCKER_PORTS := 2504
 DOCKER_WRITEABLE := true
