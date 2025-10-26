b274bdd64f781139184385efd3290b97150da1a1
C0deH4cker <c0deh4cker@gmail.com>
2025-10-25 23:09:32 -0700

    Start working on pwncc v2.3

--- Versions.mk
+++ Versions.mk
@@ -6,16 +6,16 @@
 
 # Update any time the PwnableHarness makefiles (or anything else in the pwnmake
 # image) are changed.
-PHMAKE_VERSION  := v2.3.2
-PHMAKE_RELEASED := v2.3.1
+PHMAKE_VERSION  ?= v2.3.2
+PHMAKE_RELEASED ?= v2.3.1
 
 # This only needs to update when there's a change that would affect the base
 # images. Changes that only affect PwnableHarness as a build system don't need
 # to update the base image version.
-BASE_VERSION  := v2.1
-BASE_RELEASED := v2.1
+BASE_VERSION  ?= v2.1
+BASE_RELEASED ?= v2.1
 
 # This updates slower than PWNABLEHARNESS_VERSION. It's expected that a given
 # pwncc version can be used by potentially many versions of PwnableHarness.
-PWNCC_VERSION  := v2.2
-PWNCC_RELEASED := v2.2
+PWNCC_VERSION  ?= v2.3
+PWNCC_RELEASED ?= v2.2
